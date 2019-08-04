34b58777274cfc4287baf37314df1f0576ec4f3c
Paweł Wolski <pawelwolski3@gmail.com>
2019-08-04 17:25:52 +0200

    add HandleClick in Board

--- src/index.js
+++ src/index.js
@@ -15,9 +15,6 @@ class Square extends React.Component {
   }
 }
 
-
-
-
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -26,12 +23,20 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square 
-    value={this.state.squares[i]} 
-    onClick={()=>this.handleClick(i)}
-    />;
-    }
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
 
   render() {
     const status = 'Next player: X';
@@ -40,19 +45,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
