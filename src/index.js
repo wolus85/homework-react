76515c9f458da85542f521ad3f088debbe784dae
Paweł Wolski <pawelwolski3@gmail.com>
2019-08-04 13:38:11 +0200

    add HandleClick for Swuare

--- src/index.js
+++ src/index.js
@@ -32,10 +32,12 @@ class Board extends React.Component {
     };
   }
 
-
   renderSquare(i) {
-    return <Square value={i} />;
-  }
+    return <Square 
+    value={this.state.squares[i]} 
+    onClick={()=>this.handleClick(i)}
+    />;
+    }
 
   render() {
     const status = 'Next player: X';
